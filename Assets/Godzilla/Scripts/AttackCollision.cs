4792e2697297d6b4c63691db5d77fc6330caa3c9
Anthony R Cavuoti <arc161@humboldt.edu>
2024-01-28 19:08:39 -0800

    Do begining to package up godzilla

---
+++ Assets/Godzilla/Scripts/AttackCollision.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CapsuleCollider attackCollider;
+    public GameObject destructionSpoke;
+
+    public float maxTime = 0.125f;
+    public float time = 0f;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= maxTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetType() == typeof(MeshCollider))
+        {
+            // Destroy(other.GameObject);
+            GameObject collidedObject = other.gameObject;
+            BuildingHealth buildingHealth = collidedObject.GetComponent<BuildingHealth>();
+            if (buildingHealth == null)
+            {
+                return;
+            }
+            buildingHealth.health -= 100f;
+            Vector3 center = other.bounds.center;
+            Instantiate(destructionSpoke, center, Quaternion.identity);
+            if (buildingHealth.health <= 0)
+            {
+                Destroy(collidedObject);
+            }
+        }
+        else if (other.GetType() == typeof(CapsuleCollider))
+        {
+            GameObject collidedObject = other.gameObject;
+            MechaGodzilla mechaGodzilla = collidedObject.GetComponent<MechaGodzilla>();
+            if (mechaGodzilla == null)
+            {
+                return;
+            }
+            mechaGodzilla.takeDamage(2f);
+        }
+        //speed = speed * -1;
+    }
+}
